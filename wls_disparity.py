4e7a65e57092123e28a2b2b86672188c63c82d6c
PrismaticPolygon <domwhite97@gmail.com>
2019-12-01 17:03:38 +0000

    Boy WLS looks good

--- wls_disparity.py
+++ wls_disparity.py
@@ -16,7 +16,7 @@ image_centre_w = 474.5              # rectified image centre width, in pixels
 # max_disp has to be divisable by 16.
 
 # https://docs.opencv.org/master/d2/d85/classcv_1_1StereoSGBM.html
-window_size = 3
+window_size = 5
 min_disparity = 0                # Minimum possible disparity value. Defaults to 0.
 num_disparities = 160            # Maximum disparity - minimum disparity. Defaults to 16.
 block_size = 5            # Matched blocked size. Must be an odd number >= 1. Normally between 3 and 11. Defaults to 3.
@@ -62,7 +62,14 @@ sigma = 1.2
 visual_multiplier = 1.0
 
 # http://timosam.com/python_opencv_depthimage
-wls_filter = cv2.ximgproc_DisparityWLSFilter(matcher_left=left_matcher)
+wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+
+print(wls_filter.filter)    # I don't know enough about how this works.
+
+# Just waiting for my seg fault...
+
+# Interestingly, it seems to be these two that causes the error.
+
 wls_filter.setLambda(lmbda)
 wls_filter.setSigmaColor(sigma)
 
@@ -76,6 +83,7 @@ def distance_to_image(distance):
     image = cv2.normalize(src=distance, dst=distance, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
 
     return np.uint8(image)
+
 
 def calculate(left, right, crop_disparity=True):
 
@@ -92,32 +100,118 @@ def calculate(left, right, crop_disparity=True):
 
     disparity = wls_filter.filter(disparity_left, grey_left, None, disparity_right)
 
+    print(disparity)
+
+    return disparity
+    # # Wait. What?
+    #
+    # # So it's definitely the filter itself.
+    #
     # if crop_disparity:
     #
     #     disparity = disparity[0:396, 135:1024]
-
-    return disparity_to_distance(disparity)
+    #
+    # return disparity_to_distance(disparity)
 
 
 if __name__ == "__main__":
 
     print("Running")
 
-    left = cv2.imread("images/left/1506942473.484027_L.png", cv2.IMREAD_COLOR)
-    right = cv2.imread("images/right/1506942473.484027_R.png", cv2.IMREAD_COLOR)
+    imgL = cv2.imread("images/left/1506942473.484027_L.png", cv2.IMREAD_COLOR)
+    imgR = cv2.imread("images/right/1506942473.484027_R.png", cv2.IMREAD_COLOR)
+#
+    # image = cv2.medianBlur(image, 5)
 
-    grey_left = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)  # (544, 1024)
-    grey_right = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)  # (544, 1024)
+    imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+    imgR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("Left", grey_left)
-    cv2.imshow("Right", grey_right)
+    clip_limit = 2.0
+    tile_grid_size = (8, 8)
+
+    # https://docs.opencv.org/master/d5/daf/tutorial_py_histogram_equalization.html
+    histogram_equaliser = cv2.createCLAHE(
+        clipLimit=clip_limit,  #
+        tileGridSize=tile_grid_size  #
+    )
+
+    # And crop. The near ones are throwing off the value too
+    # much.
+
+    imgL = histogram_equaliser.apply(imgL)
+    imgR = histogram_equaliser.apply(imgR)
 
     start = time.time()
 
-    distance = calculate(left, right)
+    window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
 
-    print("WLS took {:.2f} seconds".format(time.time() - start))
+    left_matcher = cv2.StereoSGBM_create(
+        minDisparity=0,
+        numDisparities=160,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+        blockSize=5,
+        P1=8 * 3 * window_size ** 2,
+        # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+        P2=32 * 3 * window_size ** 2,
+        disp12MaxDiff=1,
+        uniquenessRatio=15,
+        speckleWindowSize=0,
+        speckleRange=2,
+        preFilterCap=63,
+        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+    )
 
-    cv2.imshow("Distance", distance_to_image(distance))
+    right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
 
-    cv2.waitKey(0)
+    lmbda = 80000
+    sigma = 1.2
+    visual_multiplier = 1.0
+
+    wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+    wls_filter.setLambda(lmbda)
+    wls_filter.setSigmaColor(sigma)
+
+    print('computing disparity...')
+    displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
+    dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
+    displ = np.int16(displ)
+    dispr = np.int16(dispr)
+    filteredImg = wls_filter.filter(displ, imgL, None, dispr) # important to put "imgL" here!!!
+
+    filteredImg = filteredImg[0:396, 180:1024]
+
+    # baseline_m = 0.2090607502  # in metres
+    # focal_length_px = 399.9745178222656  # in pixels
+    # focal_length_m = 4.8 / 1000  # in metres
+    #
+    # # This is when we have the wrong types
+    #
+    # filteredImg = np.divide(focal_length_px * baseline_m, filteredImg, out=np.zeros_like(filteredImg),
+    #                         where=filteredImg > 0)
+
+    # Remember: this is disparity, NOT depth.
+    # Let's try with depth now.
+
+    filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
+    filteredImg = np.uint8(filteredImg)
+    cv2.imshow('Disparity Map', filteredImg)
+
+    cv2.imwrite("Disparity map WLS.png", filteredImg)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+    # disparity = calculate(left, right)
+    #
+    # print("WLS took {:.2f} seconds".format(time.time() - start))
+    #
+    # filtered_img = cv2.normalize(disparity, disparity, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
+    #
+    # print(filtered_img)
+    #
+    # # It's all just... white.
+    # # But it works!
+    #
+    # cv2.imshow("Distance", filtered_img)
+    #
+    # cv2.waitKey(0)
