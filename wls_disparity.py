5427435ef81611a5dbfeae282a1c57491de371d6
PrismaticPolygon <domwhite97@gmail.com>
2019-12-01 19:16:08 +0000

    Moved WLS to an OO paradigm

--- wls_disparity.py
+++ wls_disparity.py
@@ -15,195 +15,183 @@ image_centre_w = 474.5              # rectified image centre width, in pixels
 
 # max_disp has to be divisable by 16.
 
-# https://docs.opencv.org/master/d2/d85/classcv_1_1StereoSGBM.html
-window_size = 5
-min_disparity = 0                # Minimum possible disparity value. Defaults to 0.
-num_disparities = 160            # Maximum disparity - minimum disparity. Defaults to 16.
-block_size = 5            # Matched blocked size. Must be an odd number >= 1. Normally between 3 and 11. Defaults to 3.
-P1 = 8 * 1 * window_size ** 2    # First parameter controlling disparity smoothness. The penalty on the disparity change by +- 1 between pixels.
-P2 = 32 * 1 * window_size ** 2   # Second parameter controlling disparity smoothness. The larger the values, the smoother the disparity. Must be greater than P1.
-disp_12_max_diff = 1             # The maximum allowed difference (in integer pixel units) in the left-right disparity check. Set to non-positive to disable.
-pre_filter_cap = 63               # Truncation value for the prefiltered image pixels
-uniqueness_ratio = 15            # Margin in percentage by which the best (minimum) computed cost function value should "win" the second best value to consider the found match correct. Normally, a value within the 5-15 range is good enou
-speckle_window_size = 200         # The maximum size of smooth disparity regions to consider noise speckles and invalidate. Set to 0. to disable speckling. Should be somewhere between 50 - 200.
-speckle_range = 2                # maximum disparity variation within each connected component. If speckle filtering, set to positive. will be implicitly multiplied by 16. Normally, 1 or 2 is good enough.
-mode = cv2.STEREO_SGBM_MODE_HH   # Mode. Defaults to STEREO_SGBM_MODE_SGBM.
+class WLS:
 
-left_matcher = cv2.StereoSGBM_create(
-    minDisparity=min_disparity,
-    numDisparities=num_disparities,
-    blockSize=block_size,
-    P1=P1,
-    P2=P2,
-    disp12MaxDiff=disp_12_max_diff,
-    preFilterCap=pre_filter_cap,
-    uniquenessRatio=uniqueness_ratio,
-    speckleWindowSize=speckle_window_size,
-    speckleRange=speckle_range,
-    mode=mode
-)
+    def __init__(self):
 
-right_matcher = cv2.StereoSGBM_create(
-    minDisparity=min_disparity,
-    numDisparities=num_disparities,
-    blockSize=block_size,
-    P1=P1,
-    P2=P2,
-    disp12MaxDiff=disp_12_max_diff,
-    preFilterCap=pre_filter_cap,
-    uniquenessRatio=uniqueness_ratio,
-    speckleWindowSize=speckle_window_size,
-    speckleRange=speckle_range,
-    mode=mode
-)
+        print("Initialising WLS...", end="")
 
-lmbda = 80000
-sigma = 1.2
-visual_multiplier = 1.0
+        start = time.time()
 
-# http://timosam.com/python_opencv_depthimage
-wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+        # Camera variables
 
-print(wls_filter.filter)    # I don't know enough about how this works.
+        self.baseline_m = 0.2090607502  # in metres
+        self.focal_length_px = 399.9745178222656  # in pixels
+        self.focal_length_m = 4.8 / 1000  # in metres
 
-# Just waiting for my seg fault...
+        self.image_centre_h = 262.0  # rectified image centre height, in pixels
+        self.image_centre_w = 474.5  # rectified image centre width, in pixels
 
-# Interestingly, it seems to be these two that causes the error.
+        # Processor variables
 
-wls_filter.setLambda(lmbda)
-wls_filter.setSigmaColor(sigma)
+        self.window_size = 5
+        self.min_disparity = 0  # Minimum possible disparity value. Defaults to 0.
+        self.num_disparities = 160  # Maximum disparity - minimum disparity. Defaults to 16.
+        self.block_size = 5  # Matched blocked size. Must be an odd number >= 1. Normally between 3 and 11. Defaults to 3.
+        self.P1 = 8 * 1 * self.window_size ** 2  # First parameter controlling disparity smoothness. The penalty on the disparity change by +- 1 between pixels.
+        self.P2 = 32 * 1 * self.window_size ** 2  # Second parameter controlling disparity smoothness. The larger the values, the smoother the disparity. Must be greater than P1.
+        self.disp_12_max_diff = 1  # The maximum allowed difference (in integer pixel units) in the left-right disparity check. Set to non-positive to disable.
+        self.pre_filter_cap = 63  # Truncation value for the prefiltered image pixels
+        self.uniqueness_ratio = 15  # Margin in percentage by which the best (minimum) computed cost function value should "win" the second best value to consider the found match correct. Normally, a value within the 5-15 range is good enou
+        self.speckle_window_size = 200  # The maximum size of smooth disparity regions to consider noise speckles and invalidate. Set to 0. to disable speckling. Should be somewhere between 50 - 200.
+        self.speckle_range = 2  # maximum disparity variation within each connected component. If speckle filtering, set to positive. will be implicitly multiplied by 16. Normally, 1 or 2 is good enough.
+        self.mode = cv2.STEREO_SGBM_MODE_HH  # Mode. Defaults to STEREO_SGBM_MODE_SGBM.
 
-def disparity_to_distance(disparity):
+        # https://docs.opencv.org/master/d2/d85/classcv_1_1StereoSGBM.html
 
-   # https://stackoverflow.com/questions/26248654/how-to-return-0-with-divide-by-zero
-   return np.divide(focal_length_px * baseline_m, disparity, out=np.zeros_like(disparity), where=disparity > 0)
+        self.left_matcher = cv2.StereoSGBM_create(
+            minDisparity=self.min_disparity,
+            numDisparities=self.num_disparities,
+            blockSize=self.block_size,
+            P1=self.P1,
+            P2=self.P2,
+            disp12MaxDiff=self.disp_12_max_diff,
+            preFilterCap=self.pre_filter_cap,
+            uniquenessRatio=self.uniqueness_ratio,
+            speckleWindowSize=self.speckle_window_size,
+            speckleRange=self.speckle_range,
+            mode=self.mode
+        )
 
-def distance_to_image(distance):
+        self.right_matcher = cv2.ximgproc.createRightMatcher(self.left_matcher)
 
-    image = cv2.normalize(src=distance, dst=distance, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
+        # Histogram variables
 
-    return np.uint8(image)
+        self.clip_limit = 2.0
+        self.tile_grid_size = (8, 8)
 
+        # https://docs.opencv.org/master/d5/daf/tutorial_py_histogram_equalization.html
+        self.histogram_equaliser = cv2.createCLAHE(
+            clipLimit=self.clip_limit,  #
+            tileGridSize=self.tile_grid_size  #
+        )
 
-def calculate(left, right, crop_disparity=True):
+        # WLS variables
 
-    # Convert both to grayscale, as this is what the algorithm uses. Could also downscale to improve speed at the cost of quality.
+        self.lmbda = 80000
+        self.sigma = 1.2
+        self.visual_multiplier = 1.0
 
-    grey_left = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)      # (544, 1024)
-    grey_right = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)    # (544, 1024)
+        # http://timosam.com/python_opencv_depthimage
 
-    cv2.imshow("Left", grey_left)
-    cv2.imshow("Right", grey_right)
+        self.wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=self.left_matcher)
 
-    disparity_left = left_matcher.compute(grey_left, grey_right).astype(np.float32)/16
-    disparity_right = right_matcher.compute(grey_right, grey_left).astype(np.float32)/16
+        self.wls_filter.setLambda(self.lmbda)
+        self.wls_filter.setSigmaColor(self.sigma)
 
-    disparity = wls_filter.filter(disparity_left, grey_left, None, disparity_right)
+        print("DONE ({:.2f}s)".format(time.time() - start))
 
-    print(disparity)
+    def preprocess(self, left, right):
 
-    return disparity
-    # # Wait. What?
-    #
-    # # So it's definitely the filter itself.
-    #
-    # if crop_disparity:
-    #
-    #     disparity = disparity[0:396, 135:1024]
-    #
-    # return disparity_to_distance(disparity)
+        for image in (left, right):
+
+            # Convert to greyscale
+
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            image = self.histogram_equaliser.apply(image)
+
+        return left, right
+
+    def calculate(self, left, right, crop_disparity=True):
+
+        left, right = self.preprocess(left, right)
+
+        disparity_left = self.left_matcher.compute(left, right).astype(np.float32)/16
+        disparity_right = self.right_matcher.compute(right, left).astype(np.float32)/16
+
+        disparity = self.wls_filter.filter(disparity_left, left, None, disparity_right)
+
+        if crop_disparity:
+
+            disparity = disparity[0:396, 135:1024]
+
+        return disparity
+
+    def to_image(self, disparity):
+
+        image = cv2.normalize(src=disparity, dst=disparity, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
+
+        return np.uint8(image)
 
 
 if __name__ == "__main__":
 
-    print("Running")
-
     imgL = cv2.imread("images/left/1506942473.484027_L.png", cv2.IMREAD_COLOR)
     imgR = cv2.imread("images/right/1506942473.484027_R.png", cv2.IMREAD_COLOR)
-#
-    # image = cv2.medianBlur(image, 5)
 
-    imgL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
-    imgR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
-
-    clip_limit = 2.0
-    tile_grid_size = (8, 8)
-
-    # https://docs.opencv.org/master/d5/daf/tutorial_py_histogram_equalization.html
-    histogram_equaliser = cv2.createCLAHE(
-        clipLimit=clip_limit,  #
-        tileGridSize=tile_grid_size  #
-    )
-
-    # And crop. The near ones are throwing off the value too
-    # much.
-
-    imgL = histogram_equaliser.apply(imgL)
-    imgR = histogram_equaliser.apply(imgR)
+    wls = WLS()
 
     start = time.time()
 
-    window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+    disparity = wls.calculate(imgL, imgR)
 
-    left_matcher = cv2.StereoSGBM_create(
-        minDisparity=0,
-        numDisparities=160,  # max_disp has to be dividable by 16 f. E. HH 192, 256
-        blockSize=5,
-        P1=8 * 3 * window_size ** 2,
-        # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
-        P2=32 * 3 * window_size ** 2,
-        disp12MaxDiff=1,
-        uniquenessRatio=15,
-        speckleWindowSize=0,
-        speckleRange=2,
-        preFilterCap=63,
-        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
-    )
+    print("WLS took {:.2f} seconds".format(time.time() - start))
 
-    right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
+    cv2.imshow('Disparity Map', wls.to_image(disparity))
 
-    lmbda = 80000
-    sigma = 1.2
-    visual_multiplier = 1.0
+    cv2.waitKey(0)
 
-    wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
-    wls_filter.setLambda(lmbda)
-    wls_filter.setSigmaColor(sigma)
-
-    print('computing disparity...')
-    displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
-    dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
-    displ = np.int16(displ)
-    dispr = np.int16(dispr)
-    filteredImg = wls_filter.filter(displ, imgL, None, dispr) # important to put "imgL" here!!!
-
-    filteredImg = filteredImg[0:396, 180:1024]
-
-    # baseline_m = 0.2090607502  # in metres
-    # focal_length_px = 399.9745178222656  # in pixels
-    # focal_length_m = 4.8 / 1000  # in metres
+    # window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
     #
-    # # This is when we have the wrong types
+    # left_matcher = cv2.StereoSGBM_create(
+    #     minDisparity=0,
+    #     numDisparities=160,  # max_disp has to be dividable by 16 f. E. HH 192, 256
+    #     blockSize=5,
+    #     P1=8 * 3 * window_size ** 2,
+    #     P2=32 * 3 * window_size ** 2,
+    #     disp12MaxDiff=1,
+    #     uniquenessRatio=15,
+    #     speckleWindowSize=0,
+    #     speckleRange=2,
+    #     preFilterCap=63,
+    #     mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
+    # )
     #
-    # filteredImg = np.divide(focal_length_px * baseline_m, filteredImg, out=np.zeros_like(filteredImg),
-    #                         where=filteredImg > 0)
-
-    # Remember: this is disparity, NOT depth.
-    # Let's try with depth now.
-
-    filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
-    filteredImg = np.uint8(filteredImg)
-    cv2.imshow('Disparity Map', filteredImg)
-
-    cv2.imwrite("Disparity map WLS.png", filteredImg)
-
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    # right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
+    #
+    # lmbda = 80000
+    # sigma = 1.2
+    # visual_multiplier = 1.0
+    #
+    # wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
+    # wls_filter.setLambda(lmbda)
+    # wls_filter.setSigmaColor(sigma)
+    #
+    # print('computing disparity...')
+    # displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
+    # dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
+    # displ = np.int16(displ)
+    # dispr = np.int16(dispr)
+    # filteredImg = wls_filter.filter(displ, imgL, None, dispr)  # important to put "imgL" here!!!
+    #
+    # # cv2.imshow("Disparity", filteredImg)
+    # #
+    # # cv2.waitKey(0)
+    #
+    # # /filteredImg, out=np.zeros_like(filteredImg),
+    #
+    # filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
+    # filteredImg = np.uint8(filteredImg)
+    # cv2.imshow('Disparity Map', filteredImg)
+    #
+    # # cv2.imwrite("Disparity map WLS.png", filteredImg)
+    #
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
 
 
     # disparity = calculate(left, right)
-    #
-    # print("WLS took {:.2f} seconds".format(time.time() - start))
     #
     # filtered_img = cv2.normalize(disparity, disparity, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX)
     #
